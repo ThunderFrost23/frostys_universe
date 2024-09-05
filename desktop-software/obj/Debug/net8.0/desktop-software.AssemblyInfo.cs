6dcd185a97ea858b2049eec8fbaaaf7d971497cd
Ryan Lauret <ryan.lauret1@gmail.com>
2024-09-05 15:40:08 +0200

    create database (empty)

--- desktop-software/obj/Debug/net8.0/desktop-software.AssemblyInfo.cs
+++ desktop-software/obj/Debug/net8.0/desktop-software.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("desktop-software")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0aaf43404f23d77d2a59dd7f8df582b90bbbd38")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2718d00b1a11646e1e376e0b3a60dbf823721370")]
 [assembly: System.Reflection.AssemblyProductAttribute("desktop-software")]
 [assembly: System.Reflection.AssemblyTitleAttribute("desktop-software")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
