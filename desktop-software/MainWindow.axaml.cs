e8d7983da6879f872d6ade9472b5677362ea69a2
rlauret <ryan.lauret1@gmail.com>
2025-03-25 16:45:53 +0100

    Mute Logic added

--- desktop-software/MainWindow.axaml.cs
+++ desktop-software/MainWindow.axaml.cs
@@ -1,12 +1,31 @@
+using System;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 
-namespace desktop_software
-{
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
+namespace desktop_software {
+    public partial class MainWindow : Window {
+        private double _previousVolume;
+
+        public MainWindow() {
             InitializeComponent();
+            DataContext = this;
+
+            _previousVolume = VolumeSlider.Value;
+
+            // Add event handler for the mute button
+            MuteButton.Click += MuteButton_Click;
+        }
+
+        private void MuteButton_Click(object? sender, RoutedEventArgs e) {
+            if (MuteButton.IsChecked == true) {
+                // Save the current volume and mute it
+                _previousVolume = VolumeSlider.Value;
+                VolumeSlider.Value = 0;
+                
+            } else {
+                // Restore previous volume
+                VolumeSlider.Value = _previousVolume;
+            }
         }
     }
 }
